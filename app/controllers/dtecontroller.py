55ab12446103cd0fd5a74ea97eef8329af802766
Christian Rojas <christiandiegor@gmail.com>
2015-04-21 09:42:10 -0300

    correccion validacion

--- app/controllers/dtecontroller.py
+++ app/controllers/dtecontroller.py
@@ -32,6 +32,19 @@ class Formulario(webapp2.RequestHandler):
         email = self.request.get('email')
         subject = self.request.get('subject')
         htmlBody = self.request.get('htmlBody')
+        #attach = self.request.POST.get('attach', None)
+        attach = self.request.get_all('attach')
+        self.response.write(self.request.body)
+        myfiles = []
+        for att in attach:
+                # myfiles.append(att)
+            self.response.write(att)
+"""
+    def post(self):
+        campaign_id = self.request.get('campaign_id')
+        email = self.request.get('email')
+        subject = self.request.get('subject')
+        htmlBody = self.request.get('htmlBody')
         attach = self.request.POST.get('attach', None)
         att = Attachment(attach.filename, attach.file.read())
         new_mail = EmailClient()
@@ -41,6 +54,7 @@ class Formulario(webapp2.RequestHandler):
             html=htmlBody,
             attachments=att
         )
+"""
 
 
 class InputEmailHandler(webapp2.RequestHandler):
@@ -50,26 +64,29 @@ class InputEmailHandler(webapp2.RequestHandler):
         email = self.request.get('email')
         subject = self.request.get('subject')
         htmlBody = self.request.get('htmlBody')
-        url = 'http://azurian-rastreo.appspot.com/track?email=' + email + '&campaign_id=' + campaign_id
-        htmlBody = htmlBody + '<div><img src="' + urllib2.url2pathname(url) + '" width="1" height="1" border="0" /></div>'
-        attach = self.request.POST.getall('attach')
-        files = []
-        for att in attach:
-            files.append(Attachment(att.filename, att.file.read()))
-
+        url = 'http://azurian-rastreo.appspot.com/track?email=' + \
+            email + '&campaign_id=' + campaign_id
+        htmlBody = htmlBody + '<div><img src="' + \
+            urllib2.url2pathname(
+                url) + '" width="1" height="1" border="0" /></div>'
+        attach = self.request.POST.get('attach', None)
+        attachments = [Attachment(attach.filename, attach.file.read())]
+        #attachments.append(Attachment(attach.filename, attach.file.read()))
         if (campaign_id and email and subject and htmlBody):
             o_mail = Email()
-            o_mail.campaign_id = campaign_id
-            o_mail.email = email
-            o_mail.subject = subject
-            o_mail.htmlBody = htmlBody
-            o_mail.put()
+            result = o_mail.search_email(email, campaign_id)
+            if not result:
+                o_mail.campaign_id = campaign_id
+                o_mail.email = email
+                o_mail.subject = subject
+                o_mail.htmlBody = htmlBody
+                o_mail.put()
             new_mail = EmailClient()
             new_mail.create_mail(
-                to=o_mail.email,
-                subject=o_mail.subject,
-                html=o_mail.htmlBody,
-                attachments=files
+                to=email,
+                subject=subject,
+                html=htmlBody,
+                attachments=attachments
             )
         else:
             self.response.write('Error')
