4d01684ad335f8f410b305e3e1d8115eea0aa2d8
Christian Rojas <christiandiegor@gmail.com>
2015-05-18 12:58:17 -0300

    errors handler

--- app/controllers/errorhandler_controller.py
+++ app/controllers/errorhandler_controller.py
@@ -3,16 +3,21 @@
 
 
 import webapp2
+import logging
 
 
 from config.jinja_environment import JINJA_ENVIRONMENT
 
 
 def handle_404(request, response, exception):
+	logging.exception(exception)
 	template = JINJA_ENVIRONMENT.get_template('404.html')
 	response.write(template.render())
+	response.set_status(404)
 
 
 def handle_500(request, response, exception):
+	logging.exception(exception)
 	template = JINJA_ENVIRONMENT.get_template('500.html')
 	response.write(template.render())
+	response.set_status(500)
