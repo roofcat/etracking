7f36faf048be4918c3d6cf2d4e676b0dc97e62d2
Christian Rojas <christiandiegor@gmail.com>
2015-07-30 10:57:54 -0300

    ya se envian correo y funciona webhook

--- app/controllers/dteinput_controller.py
+++ app/controllers/dteinput_controller.py
@@ -4,6 +4,7 @@
 
 import webapp2
 import logging
+import datetime
 
 
 from google.appengine.api import taskqueue
@@ -23,8 +24,8 @@ class InputEmailHandler(webapp2.RequestHandler):
 
     def post(self):
         # registro de header y body del request
-        logging.info(self.request.headers)
-        logging.info(self.request.body)
+        #logging.info(self.request.headers)
+        #logging.info(self.request.body)
         # seteo de parametros recibidos
         empresa = self.request.get('empresa')
         rut_receptor = self.request.get('rut_receptor')
@@ -48,6 +49,8 @@ class InputEmailHandler(webapp2.RequestHandler):
         adjunto1 = self.request.POST.get('adjunto1', None)
         adjunto2 = self.request.POST.get('adjunto2', None)
         adjunto3 = self.request.POST.get('adjunto3', None)
+        logging.info(fecha_emision)
+        logging.info(fecha_recepcion)
 
         if (correo and asunto and html and numero_folio and tipo_dte):
             # Proceso de creación del objeto email
@@ -55,51 +58,66 @@ class InputEmailHandler(webapp2.RequestHandler):
             email_result = my_email.search_email(
                 correo, numero_folio, tipo_dte)
             if email_result == None:
-                try:
+                #try:
+                my_email.nombre_cliente = nombre_cliente
+                my_email.correo = correo
+                my_email.asunto = asunto
+                my_email.html = html
+                my_email.tipo_dte = tipo_dte
+                my_email.numero_folio = numero_folio
+                if empresa:
                     my_email.empresa = empresa
+                if rut_receptor:
                     my_email.rut_receptor = rut_receptor
+                if rut_emisor:
                     my_email.rut_emisor = rut_emisor
+                if tipo_envio:
                     my_email.tipo_envio = tipo_envio
-                    my_email.tipo_dte = tipo_dte
-                    my_email.numero_folio = numero_folio
+                if resolucion_receptor:
                     my_email.resolucion_receptor = resolucion_receptor
+                if resolucion_emisor:
                     my_email.resolucion_emisor = resolucion_emisor
-                    my_email.monto = monto
-                    my_email.fecha_emision = fecha_emision
-                    my_email.fecha_recepcion = fecha_recepcion
+                if monto:
+                    try:
+                        my_email.monto = int(monto, base=10)
+                    except:
+                        pass
+                if fecha_emision and not None:
+                    my_email.fecha_emision = datetime.datetime.fromtimestamp(float(fecha_emision))
+                if fecha_recepcion and not None:
+                    my_email.fecha_recepcion = datetime.datetime.fromtimestamp(float(fecha_recepcion))
+                if estado_documento:
                     my_email.estado_documento = estado_documento
+                if tipo_operacion:
                     my_email.tipo_operacion = tipo_operacion
+                if tipo_receptor:
                     my_email.tipo_receptor = tipo_receptor
-                    my_email.nombre_cliente = nombre_cliente
-                    my_email.correo = correo
-                    my_email.asunto = asunto
-                    my_email.html = html
-                    # Evaluar adjuntos
-                    if not adjunto1 == None:
-                        att = AttachModel(
-                            name=adjunto1.filename, attach=adjunto1.file.read()).put()
-                        my_email.attachs.append(att)
-                    if not adjunto2 == None:
-                        att = AttachModel(
-                            name=adjunto2.filename, attach=adjunto2.file.read()).put()
-                        my_email.attachs.append(att)
-                    if not adjunto3 == None:
-                        att = AttachModel(
-                            name=adjunto3.filename, attach=adjunto3.file.read()).put()
-                        my_email.attachs.append(att)
-                    my_email.put()
-                    context = {
-                        'correo': correo,
-                        'numero_folio': numero_folio,
-                        'tipo_dte': tipo_dte,
-                    }
-                    # Inicio taskqueue
-                    q = taskqueue.Queue('InputQueue')
-                    t = taskqueue.Task(url='/inputqueue', params=context)
-                    q.add(t)
-                    self.response.write({'message': 'success'})
-                except Exception, e:
-                    logging.error(e)
+                # Evaluar adjuntos
+                if not adjunto1 == None:
+                    att = AttachModel(
+                        nombre=adjunto1.filename, archivo=adjunto1.file.read()).put()
+                    my_email.attachs.append(att)
+                if not adjunto2 == None:
+                    att = AttachModel(
+                        nombre=adjunto2.filename, archivo=adjunto2.file.read()).put()
+                    my_email.attachs.append(att)
+                if not adjunto3 == None:
+                    att = AttachModel(
+                        nombre=adjunto3.filename, archivo=adjunto3.file.read()).put()
+                    my_email.attachs.append(att)
+                my_email.put()
+                context = {
+                    'correo': correo,
+                    'numero_folio': numero_folio,
+                    'tipo_dte': tipo_dte,
+                }
+                # Inicio taskqueue
+                q = taskqueue.Queue('InputQueue')
+                t = taskqueue.Task(url='/inputqueue', params=context)
+                q.add(t)
+                self.response.write({'message': 'success'})
+                #except Exception, e:
+                #    logging.error(e)
             else:
                 logging.info('objeto ya existe')
             # fin todo
