de2ca81648ae0d8d7ddb96d0c8eac9a0a9cfc8e7
Christian Rojas <christiandiegor@gmail.com>
2015-10-22 18:44:55 -0300

    creando reportes en background

--- app/controllers/export_controller.py
+++ app/controllers/export_controller.py
@@ -2,11 +2,9 @@
 #!/usr/bin/env python
 
 
-import codecs
-import csv
 import datetime
 import logging
-import StringIO
+import json
 import webapp2
 
 
@@ -34,50 +32,124 @@ class QueueExportHandler(webapp2.RequestHandler):
             y enviarlos como link al correo del usuario
     """
 
-    def post(self):
-        export_type = self.request.get('export_type')
-        if export_type == 'export_general_email':
-            options = self.request.get('options')
-            user_email = self.request.get('user_email')
-            file_name = self.request.get('file_name')
-            date_from = self.request.get('date_from')
-            date_to = self.request.get('date_to')
-            date_from = int(date_from, base=10)
-            date_to = int(date_to, base=10)
-            date_from = datetime.datetime.fromtimestamp(date_from)
-            date_to = datetime.datetime.fromtimestamp(date_to)
-            # Consulta
-            data = EmailModel.get_all_emails_by_dates(date_from, date_to, options)
-            # Creación del documento
-            doc_export = create_tablib(data)
-            # Buscar el objeto usuario
-            user = UserModel.get_user(user_email)
-            # Creación de objeto reporte
-            report = ExportModel()
-            report.name = file_name
-            report.export_file = doc_export.xlsx
-            report.user = user.email
-            report.put()
-            # actualizar campo
-            report.url = REPORT_EXPORT_URL.format(url_safe=report.key.urlsafe())
-            report.put()
-            # Proceso de correo
-            mail = EmailClient()
-            mail_report = {
-                'email': user.email,
-                'user_name': user.first_name,
-                'subject': REPORT_SUBJECT_MAIL,
-                'html': REPORT_HTML_MAIL.format(user_name=user.first_name, report_link=report.url),
-            }
-            mail.send_user_email(mail_report)
-        elif export_type == '':
-            pass
-        elif export_type == '':
-            pass
-        elif export_type == '':
-            pass
-        elif export_type == '':
-            pass
+	def post(self):
+		export_type = self.request.get('export_type')
+		if export_type == 'export_general_email':
+			options = self.request.get('options')
+			user_email = self.request.get('user_email')
+			file_name = self.request.get('file_name')
+			date_from = self.request.get('date_from')
+			date_to = self.request.get('date_to')
+			date_from = int(date_from, base=10)
+			date_to = int(date_to, base=10)
+			date_from = datetime.datetime.fromtimestamp(date_from)
+			date_to = datetime.datetime.fromtimestamp(date_to)
+			# Consulta
+			data = EmailModel.get_all_emails_by_dates(date_from, date_to, options)
+		elif export_type == 'export_sended_email':
+			options = self.request.get('options')
+			user_email = self.request.get('user_email')
+			file_name = self.request.get('file_name')
+			date_from = self.request.get('date_from')
+			date_to = self.request.get('date_to')
+			date_from = int(date_from, base=10)
+			date_to = int(date_to, base=10)
+			date_from = datetime.datetime.fromtimestamp(date_from)
+			date_to = datetime.datetime.fromtimestamp(date_to)
+			# Consulta
+			data = EmailModel.get_all_sended_emails_by_dates(date_from, date_to, options)
+		elif export_type == 'export_failure_email':
+			options = self.request.get('options')
+			user_email = self.request.get('user_email')
+			file_name = self.request.get('file_name')
+			date_from = self.request.get('date_from')
+			date_to = self.request.get('date_to')
+			date_from = int(date_from, base=10)
+			date_to = int(date_to, base=10)
+			date_from = datetime.datetime.fromtimestamp(date_from)
+			date_to = datetime.datetime.fromtimestamp(date_to)
+			# Consulta
+			data = EmailModel.get_all_failure_emails_by_dates(date_from, date_to, options)
+		elif export_type == 'export_search_by_email':
+			email = self.request.get('email')
+			email = str(email).lower()
+			user_email = self.request.get('user_email')
+			file_name = self.request.get('file_name')
+			date_from = self.request.get('date_from')
+			date_to = self.request.get('date_to')
+			date_from = int(date_from, base=10)
+			date_to = int(date_to, base=10)
+			date_from = datetime.datetime.fromtimestamp(date_from)
+			date_to = datetime.datetime.fromtimestamp(date_to)
+			# Consulta
+			data = EmailModel.get_info_by_email(date_from, date_to, email)
+		elif export_type == 'export_search_by_folio':
+			folio = self.request.get('folio')
+			folio = str(folio).lower()
+			user_email = self.request.get('user_email')
+			file_name = self.request.get('file_name')
+			# Consulta
+			data = EmailModel.get_emails_by_folio(folio)
+		elif export_type == 'export_search_by_rut':
+			rut = self.request.get('rut')
+			rut = str(rut).upper()
+			user_email = self.request.get('user_email')
+			file_name = self.request.get('file_name')
+			date_from = self.request.get('date_from')
+			date_to = self.request.get('date_to')
+			date_from = int(date_from, base=10)
+			date_to = int(date_to, base=10)
+			date_from = datetime.datetime.fromtimestamp(date_from)
+			date_to = datetime.datetime.fromtimestamp(date_to)
+			# Consulta
+			data = EmailModel.get_emails_by_rut_receptor(date_from, date_to, rut)
+		elif export_type == 'export_search_by_failure':
+			user_email = self.request.get('user_email')
+			file_name = self.request.get('file_name')
+			date_from = self.request.get('date_from')
+			date_to = self.request.get('date_to')
+			date_from = int(date_from, base=10)
+			date_to = int(date_to, base=10)
+			date_from = datetime.datetime.fromtimestamp(date_from)
+			date_to = datetime.datetime.fromtimestamp(date_to)
+			# Consulta
+			data = EmailModel.get_all_failure_emails_by_dates(date_from, date_to)
+		elif export_type == 'export_search_by_mount':
+			mount_from = self.request.get('mount_from')
+			mount_to = self.request.get('mount_to')
+			mount_from = int(mount_from, base=10)
+			mount_to = int(mount_to, base=10)
+			file_name = self.request.get('file_name')
+			date_from = self.request.get('date_from')
+			date_to = self.request.get('date_to')
+			date_from = int(date_from, base=10)
+			date_to = int(date_to, base=10)
+			date_from = datetime.datetime.fromtimestamp(date_from)
+			date_to = datetime.datetime.fromtimestamp(date_to)
+			# Consulta
+			data = EmailModel.get_emails_by_mount(date_from, date_to, mount_from, mount_to)
+		# Creación del documento
+		doc_export = create_tablib(data)
+		# Buscar el objeto usuario
+		user = UserModel.get_user(user_email)
+		# Creación de objeto reporte
+		report = ExportModel()
+		report.name = file_name
+		report.export_file = doc_export.xlsx
+		report.user = user.email
+		report.put()
+		# actualizar campo
+		report.url = REPORT_EXPORT_URL.format(url_safe=report.key.urlsafe())
+		report.put()
+		# Proceso de correo
+		mail = EmailClient()
+		mail_report = {
+			'email': user.email,
+			'user_name': user.first_name,
+			'subject': REPORT_SUBJECT_MAIL,
+			'html': REPORT_HTML_MAIL.format(user_name=user.first_name, report_link=report.url),
+		}
+		mail.send_user_email(mail_report)
 
 
 """ Serie de clases controladoras que reciben parametros
@@ -104,120 +176,158 @@ class ExportGeneralEmailHandler(BaseHandler):
 			q = taskqueue.Queue('ReportQueue')
 			t = taskqueue.Task(url='/exportqueue', params=context)
 			q.add(t)
+			self.response.write(json.dumps({'status': 'ok'}))
+		else:
+			self.response.write(json.dumps({'status': 'no'}))
 
 
 class ExportSendedEmailHandler(BaseHandler):
 
-    def get(self, date_from, date_to, options):
-
-        if date_from and date_to and options:
-            try:
-                date_from = int(date_from, base=10)
-                date_to = int(date_to, base=10)
-                date_from = datetime.datetime.fromtimestamp(date_from)
-                date_to = datetime.datetime.fromtimestamp(date_to)
-                # Consulta
-                query = EmailModel.get_all_sended_emails_by_dates(
-                    date_from, date_to, options)
-                create_csv(self, query, 'reporte_enviados.csv')
-            except Exception, e:
-                logging.info(e)
+	def get(self, date_from, date_to, options):
+		if date_from and date_to and options:
+			user = self.session['user']
+			context = {
+				'date_from': int(date_from, base=10),
+				'date_to': int(date_to, base=10),
+				'options': options,
+				'user_email': user['email'],
+				'file_name': 'reporte_enviados.xlsx',
+				'export_type': 'export_sended_email',
+			}
+			# Inicio taskqueue
+			q = taskqueue.Queue('ReportQueue')
+			t = taskqueue.Task(url='/exportqueue', params=context)
+			q.add(t)
+			self.response.write(json.dumps({'status': 'ok'}))
+		else:
+			self.response.write(json.dumps({'status': 'no'}))
 
 
 class ExportFailureEmailHandler(BaseHandler):
 
-    def get(self, date_from, date_to, options):
-
-        if date_from and date_to and options:
-            try:
-                date_from = int(date_from, base=10)
-                date_to = int(date_to, base=10)
-                date_from = datetime.datetime.fromtimestamp(date_from)
-                date_to = datetime.datetime.fromtimestamp(date_to)
-                # Consulta
-                query = EmailModel.get_all_failure_emails_by_dates(
-                    date_from, date_to, options)
-                create_csv(self, query, 'reporte_fallidos.csv')
-            except Exception, e:
-                logging.info(e)
+	def get(self, date_from, date_to, options):
+		if date_from and date_to and options:
+			user = self.session['user']
+			context = {
+				'date_from': int(date_from, base=10),
+				'date_to': int(date_to, base=10),
+				'options': options,
+				'user_email': user['email'],
+				'file_name': 'reporte_fallidos.xlsx',
+				'export_type': 'export_failure_email',
+			}
+			# Inicio taskqueue
+			q = taskqueue.Queue('ReportQueue')
+			t = taskqueue.Task(url='/exportqueue', params=context)
+			q.add(t)
+			self.response.write(json.dumps({'status': 'ok'}))
+		else:
+			self.response.write(json.dumps({'status': 'no'}))
 
 
 class ExportSearchByEmailHandler(BaseHandler):
 
     def get(self, date_from, date_to, email):
-
         if date_from and date_to and email:
-            try:
-                date_from = int(date_from, base=10)
-                date_to = int(date_to, base=10)
-                date_from = datetime.datetime.fromtimestamp(date_from)
-                date_to = datetime.datetime.fromtimestamp(date_to)
-                email = str(email).lower()
-                query = EmailModel.get_info_by_email(date_from, date_to, email)
-                create_csv(self, query, 'reporte_por_email.csv')
-            except Exception, e:
-                logging.info(e)
+        	user = self.session['user']
+			context = {
+				'date_from': int(date_from, base=10),
+				'date_to': int(date_to, base=10),
+				'email': email,
+				'user_email': user['email'],
+				'file_name': 'reporte_por_email.xlsx',
+				'export_type': 'export_search_by_email',
+			}
+			# Inicio taskqueue
+			q = taskqueue.Queue('ReportQueue')
+			t = taskqueue.Task(url='/exportqueue', params=context)
+			q.add(t)
+			self.response.write(json.dumps({'status': 'ok'}))
+		else:
+			self.response.write(json.dumps({'status': 'no'}))
 
 
 class ExportSearchByFolioHandler(BaseHandler):
 
     def get(self, folio):
         if folio:
-            try:
-                folio = str(folio)
-                query = EmailModel.get_emails_by_folio(folio)
-                logging.info(query)
-                create_csv(self, query, 'reporte_por_folio.csv')
-            except Exception, e:
-                logging.info(e)
+        	user = self.session['user']
+			context = {
+				'folio': str(folio),
+				'user_email': user['email'],
+				'file_name': 'reporte_por_folio.xlsx',
+				'export_type': 'export_search_by_folio',
+			}
+			# Inicio taskqueue
+			q = taskqueue.Queue('ReportQueue')
+			t = taskqueue.Task(url='/exportqueue', params=context)
+			q.add(t)
+			self.response.write(json.dumps({'status': 'ok'}))
+		else:
+			self.response.write(json.dumps({'status': 'no'}))
 
 
 class ExportSearchByRutHandler(BaseHandler):
 
-    def get(self, date_from, date_to, rut):
-        if date_from and date_to and rut:
-            try:
-                date_from = int(date_from, base=10)
-                date_to = int(date_to, base=10)
-                date_from = datetime.datetime.fromtimestamp(date_from)
-                date_to = datetime.datetime.fromtimestamp(date_to)
-                rut = str(rut).upper()
-                query = EmailModel.get_emails_by_rut_receptor(
-                    date_from, date_to, rut)
-                create_csv(self, query, 'reporte_por_rut.csv')
-            except Exception, e:
-                logging.info(e)
+	def get(self, date_from, date_to, rut):
+		if date_from and date_to and rut:
+			user = self.session['user']
+			context = {
+				'date_from': int(date_from, base=10),
+				'date_to': int(date_to, base=10),
+				'rut': rut,
+				'user_email': user['email'],
+				'file_name': 'reporte_por_rut.xlsx',
+				'export_type': 'export_search_by_rut',
+			}
+			# Inicio taskqueue
+			q = taskqueue.Queue('ReportQueue')
+			t = taskqueue.Task(url='/exportqueue', params=context)
+			q.add(t)
+			self.response.write(json.dumps({'status': 'ok'}))
+		else:
+			self.response.write(json.dumps({'status': 'no'}))
 
 
 class ExportSearchByFailureHandler(BaseHandler):
 
     def get(self, date_from, date_to):
         if date_from and date_to:
-            try:
-                date_from = int(date_from, base=10)
-                date_to = int(date_to, base=10)
-                date_from = datetime.datetime.fromtimestamp(date_from)
-                date_to = datetime.datetime.fromtimestamp(date_to)
-                query = EmailModel.get_all_failure_emails_by_dates(
-                    date_from, date_to)
-                create_csv(self, query, 'reporte_fallidos.csv')
-            except Exception, e:
-                logging.info(e)
+        	user = self.session['user']
+			context = {
+				'date_from': int(date_from, base=10),
+				'date_to': int(date_to, base=10),
+				'user_email': user['email'],
+				'file_name': 'reporte_fallidos.xlsx',
+				'export_type': 'export_search_by_failure',
+			}
+			# Inicio taskqueue
+			q = taskqueue.Queue('ReportQueue')
+			t = taskqueue.Task(url='/exportqueue', params=context)
+			q.add(t)
+			self.response.write(json.dumps({'status': 'ok'}))
+		else:
+			self.response.write(json.dumps({'status': 'no'}))
 
 
 class ExportSearchByMountHandler(BaseHandler):
 
-    def get(self, date_from, date_to, mount_from, mount_to):
-        if date_from and date_to:
-            try:
-                date_from = int(date_from, base=10)
-                date_to = int(date_to, base=10)
-                date_from = datetime.datetime.fromtimestamp(date_from)
-                date_to = datetime.datetime.fromtimestamp(date_to)
-                mount_from = int(mount_from, base=10)
-                mount_to = int(mount_to, base=10)
-                data = EmailModel.get_emails_by_mount(
-                    date_from, date_to, mount_from, mount_to)
-                create_csv(self, query, 'reporte_por_monto.csv')
-            except Exception, e:
-                logging.info(e)
+	def get(self, date_from, date_to, mount_from, mount_to):
+		if date_from and date_to:
+			user = self.session['user']
+			context = {
+				'date_from': int(date_from, base=10),
+				'date_to': int(date_to, base=10),
+				'mount_from': int(mount_from, base=10),
+				'mount_to': int(mount_to, base=10),
+				'user_email': user['email'],
+				'file_name': 'reporte_por_monto.xlsx',
+				'export_type': 'export_search_by_mount',
+			}
+			# Inicio taskqueue
+			q = taskqueue.Queue('ReportQueue')
+			t = taskqueue.Task(url='/exportqueue', params=context)
+			q.add(t)
+			self.response.write(json.dumps({'status': 'ok'}))
+		else:
+			self.response.write(json.dumps({'status': 'no'}))
